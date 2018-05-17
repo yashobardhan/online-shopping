6c28a83e21a350545e95910da560665693320d87
yashobardhan <yashobardhanbhardwaj@gmail.com>
2018-05-17 23:45:56 +0530

    spring mvc jars add modeland view add

---
+++ onlineshopping/src/main/java/com/shop/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "yash Welcome to spring mvc");
+		return mv;
+	}
+}
